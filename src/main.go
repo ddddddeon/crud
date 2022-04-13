3c1ba723047eea16dedbac84a695a5944b057d67
ddddddeon <opfunk@gmail.com>
2022-04-12 20:50:42 -0400

    test

--- src/main.go
+++ src/main.go
@@ -13,7 +13,7 @@ func main() {
 	r.GET("/users", func(c *gin.Context) {
 		c.JSON(200, users)
 	})
-	// `hiiiiiiii
+	// `hiiiiiiiii
 
 	r.GET("/users/:id", func(c *gin.Context) {
 		id, err := strconv.Atoi(c.Param("id"))
